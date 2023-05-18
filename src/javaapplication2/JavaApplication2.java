8c52b23ab70d83a9ff250ca7eded7c3f8ded476f
Abdulbasit <sorguly9991@gmail.com>
2023-05-18 11:53:30 -0400

    my first commit

--- src/javaapplication2/JavaApplication2.java
+++ src/javaapplication2/JavaApplication2.java
@@ -14,6 +14,7 @@ public class JavaApplication2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("hello abd");
         // TODO code application logic here
     }
     
